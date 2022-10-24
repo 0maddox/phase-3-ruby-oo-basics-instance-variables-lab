42b009a442835562061db75df9a77831b7ade036
nicholas <nickkiim7@gmail.com>
2022-10-24 22:50:43 +0300

    completed assighnment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    def name= (dog_name)
+      @this_dogs_name = dog_name
+    end
 
-end
+    def name
+      @this_dogs_name
+    end
+  end
+
+snoopy = Dog.new
+snoopy.name = "Snoopy"
+
+puts snoopy.name
+
